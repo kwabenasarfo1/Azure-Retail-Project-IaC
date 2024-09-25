5ac222f9cf9c56526499d9685ac4610a03bd84b2
John Osei-Ansah <johnoseiansah.com>
2024-09-25 10:01:13 +0100

    kubelet identity used

--- deployment/terraform/aks/main.tf
+++ deployment/terraform/aks/main.tf
@@ -34,7 +34,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   network_profile {
     network_plugin    = "azure"
     network_policy    = "azure"
-    load_balancer_sku = "Standard"
+    load_balancer_sku = "standard"
   }
 }
 
